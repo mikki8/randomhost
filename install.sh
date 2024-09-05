1d61f4c80bdf2cdc206cf99daf0d95ca829520f2
mikki8 <76606753+mikki8@users.noreply.github.com>
2024-09-05 12:05:57 +0200

    Update install.sh
    cool changes

--- install.sh
+++ install.sh
@@ -1,4 +1,14 @@
+#!/bin/bash
+
 sudo -v
+
+#https://stackoverflow.com/a/20437297
+command -v git >/dev/null 2>&1 ||
+{ echo >&2 "Git is not installed. Installing..";
+  sudo apt install git -y
+}
+
+
 git clone https://github.com/ryanmroth/randomhost.git
 sudo cp randomhost/randomhost /usr/bin
 sudo chmod +x /usr/bin/randomhost
@@ -6,3 +16,4 @@ sudo cp randomhost/randomhost.service /etc/systemd/system
 sudo chmod 644 /etc/systemd/system/randomhost.service
 sudo systemctl daemon-reload 
 sudo systemctl enable randomhost
+sudo rm -r randomhost
